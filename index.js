6356a5db3a5d78d2c99df87fcd72d156acbe4dab
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-17 20:18:59 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function fooo(){
+    return 'bar';
+}
+
+const foo= function(){
+    return 'bar';
+}
+const add=(parameter1,parameter2)=>parameter1+parameter2;
+console.log(add(2,3));
+
+const divide=function(){
+    return(2000/100);
+}
+
+const square= X => X*X;
